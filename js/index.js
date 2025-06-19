1fefd68fc0618faab1a8524d5aa0b47323fb2097
Miguel Arce <159281661+Mikearca@users.noreply.github.com>
2025-06-19 15:55:29 -0500

    Home
    Se creo la interfaz de inicio y se agrego funcionalidad al login

--- js/index.js
+++ js/index.js
@@ -9,3 +9,59 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  // REGISTRO
+  document.getElementById("registerForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    
+    const nombre = document.getElementById("registerName").value;
+    const email = document.getElementById("registerEmail").value;
+    const password = document.getElementById("registerPassword").value;
+
+    const data = {
+      nombre,
+      email,
+      contrasenaHash: password,
+      rolID: 1  
+    };
+
+    const res = await fetch("https://localhost:7290/api/account/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const result = await res.json();
+    alert(result.message);
+  });
+
+  // LOGIN
+  document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+
+    const data = {
+      email,
+      password
+    };
+
+    const res = await fetch("https://localhost:7290/api/account/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const result = await res.json();
+
+    if (res.ok) {
+
+      window.location.href = "../views/home.html";
+
+    } else {
+      alert(result.message);
+    }
+  });
+});
